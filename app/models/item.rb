8b17f96fcc515a984021772cce55a050261d5c63
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 18:06:55 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
 
   validates :image, presence: { message: 'を添付してください' }
   validates :name, presence: true
+  validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
