4bde37e2868237a180d8773f021d38891b1d0013
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 23:33:59 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_28_141448) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_142731) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_28_141448) do
     t.integer "shipping_fee_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "shipping_date_id", null: false
-    t.integer "content_id", null: false
     t.integer "price", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
