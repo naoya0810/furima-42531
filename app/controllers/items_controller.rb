4523965d9ee556be89c53bacd7c8981c2c71a524
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 17:45:34 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :shipping_fee_id, :prefecture_id, :shipping_date_id,
+                                 :content_id, :price, :image)
   end
 end
