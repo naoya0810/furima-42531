85dc5902580c8c4919881b56a7ef6ceeeaef2a03
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 21:57:40 +0900

    Update 20250526120822_create_items.rb

--- db/migrate/20250526120822_create_items.rb
+++ db/migrate/20250526120822_create_items.rb
@@ -4,6 +4,7 @@ class CreateItems < ActiveRecord::Migration[7.1]
       t.string :name,            null: false
       t.text :description,       null: false
       t.integer :category_id,     null: false
+      t.integer :condition_id,    null: false
       t.integer :shipping_fee_id, null: false
       t.integer :prefecture_id,   null: false
       t.integer :shipping_date_id,null: false
