5cb6b504f499efe977e1bc3a37a400e6d250a5dd
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 20:43:37 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,13 @@ class Item < ApplicationRecord
   validates :image, presence: { message: 'を添付してください' }
   validates :name, presence: true
   validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :condition_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_date_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                    message: 'は¥300〜9,999,999の間で入力してください' }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, message: 'は半角数字で入力してください' }
 end
