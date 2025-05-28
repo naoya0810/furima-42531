3d712b1ae60bc7cb691ef220af4fd1952581cb29
naoya0810 <naomiti.yamada@gmail.com>
2025-05-28 17:17:06 +0900

    Update 20250526120822_create_items.rb

--- db/migrate/20250526120822_create_items.rb
+++ db/migrate/20250526120822_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
+      t.string :name,            null: false
+      t.text :description,       null: false
+      t.integer :category_id,     null: false
+      t.integer :shipping_fee_id, null: false
+      t.integer :prefecture_id,   null: false
+      t.integer :shipping_date_id,null: false
+      t.integer :content_id,      null: false
+      t.integer :price,           null: false
+      t.references :user,         foreign_key: true
 
       t.timestamps
     end
