eb684a8c46231f955964006743ef4aba43621860
naoya0810 <naomiti.yamada@gmail.com>
2025-06-02 17:48:06 +0900

    マイグレーションファイル変更・削除

--- db/migrate/20250528142731_remove_content_id_from_items.rb
+++
@@ -1,9 +0,0 @@
-class RemoveContentIdFromItems < ActiveRecord::Migration[7.1]
-  def up
-    remove_column :items, :content_id
-  end
-
-  def down
-    add_column :items, :content_id, :integer
-  end
-end
