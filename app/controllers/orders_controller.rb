136c81471543d2d894ccd2c66a770ce967de0c2f
naoya0810 <naomiti.yamada@gmail.com>
2025-06-03 15:51:23 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
