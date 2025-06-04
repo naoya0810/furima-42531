9fc1621075ebbb0869cf6373871231a2e2d386fe
naoya0810 <naomiti.yamada@gmail.com>
2025-06-04 13:22:23 +0900

    Update order_form.rb

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -20,15 +20,5 @@ class OrderForm
   validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
 
   def save
-    # order = Order.create(user_id: user_id, item_id: item_id)
-    # Address.create(
-    #   postal_code: postal_code,
-    #   prefecture_id: prefecture_id,
-    #   city: city,
-    #   addresses: addresses,
-    #   building: building,
-    #   phone_number: phone_number,
-    #   order_id: order.id
-    # )
+    true
   end
-end
