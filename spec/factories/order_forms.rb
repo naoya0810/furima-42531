81748fcd627857f1e22678c1e69c058dc2a4913e
naoya0810 <naomiti.yamada@gmail.com>
2025-06-04 18:52:46 +0900

    Create order_forms.rb

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_form do
+    token { 'tok_abcdefghijk00000000000000000' }
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '渋谷区' }
+    addresses { '1-1' }
+    building { 'テストビル' }
+    phone_number { '09012345678' }
+
+    association :user
+    association :item
+  end
+end
