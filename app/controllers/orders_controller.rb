7fb16c8f34eedace37b812509ee46b2ab31362c9
naoya0810 <naomiti.yamada@gmail.com>
2025-06-05 15:18:36 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,18 +6,19 @@ class OrdersController < ApplicationController
 
   def index
     @order_form = OrderForm.new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # ← 環境変数から秘密鍵をセット
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
       Payjp::Charge.create(
-        amount: @item.price,                   # 金額（商品価格）
-        card: order_params[:token], # カードトークン
-        currency: 'jpy'                        # 通貨単位
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
       )
-      @order_form.save                         # 注文と配送先の保存
+      @order_form.save
       redirect_to root_path
     else
       render :index
