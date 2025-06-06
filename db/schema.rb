4071a95dd5b8832bad71e845517cf734aa6581ef
naoya0810 <naomiti.yamada@gmail.com>
2025-06-06 12:39:52 +0900

    building追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_114530) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_06_033320) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_114530) do
     t.bigint "order_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "building"
     t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
